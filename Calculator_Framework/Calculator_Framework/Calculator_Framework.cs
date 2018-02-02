876c7492e6661c281639108fbc2138aeea419716
Daniel Hansen <dph1108@gmail.com>
2018-02-02 16:57:46 +0100

    Har added Accumulator... Ved bare ikke hvilken udvidelser den skal have... Eller om den skal være en slags nedarvning fra alt det andet. Afprøv den, det er Calculator_Framework.cs output der virker.

--- Calculator_Framework/Calculator_Framework/Calculator_Framework.cs
+++ Calculator_Framework/Calculator_Framework/Calculator_Framework.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace Calculator_Framework
 {
@@ -37,12 +38,27 @@ namespace Calculator_Framework
             return dividend / divisor;
         }
 
+        //Så vi skal lave lidt get set syntaks, det er en anden metode at lave get og set funktionen
+        public double Accumulator //Lav property 
+        { get; private set; } //Bliver set som get{return Accumulator;} og set {Accumulator = værdi;}
+        
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to ze Calculator, yes yes: ");
 
             double a = 10, b = 0, c = 10, d = -2;
+            
             var result = new Calculator();
+
+            string number = Console.ReadLine(); //ReadLine er strings... den skal laves om til double
+            double newNumber = Convert.ToDouble(number); //Lav numret om til en double
+            double f = result.Accumulator = newNumber; //Accumulator modtager et nyt nummer
+
+            Console.WriteLine($"{f}"); //Et output af accumulatoreren
+            
+            var summing = result.Add(f, f); //Laver en sum med den accumulatoreren virker
+            
+            Console.WriteLine($"{f} + {f} = {summing}"); //Se det virker!
 
             try
             {
