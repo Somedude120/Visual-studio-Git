0039f5ec83eaf689a6922bf2e65dd6096f33364a
Emil <pehmil@gmail.com>
2018-02-15 11:13:53 +0100

    ECS added

---
+++ ECS Legacy Solution/ECS.Legacy/TempSensor.cs
@@ -0,0 +1,15 @@
+﻿namespace ECS.Legacy
+{
+    internal class TempSensor
+    {
+        public int GetTemp()
+        {
+            return 25;
+        }
+
+        public bool RunSelfTest()
+        {
+            return true;
+        }
+    }
+}
