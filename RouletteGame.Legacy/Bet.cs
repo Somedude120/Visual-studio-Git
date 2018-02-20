b16eccdced3267bbfe24e461e853d623165908f7
Daniel Hansen <dph1108@gmail.com>
2018-02-20 14:56:00 +0100

    Tilsat Roulette mappen

---
+++ RouletteGame.Legacy/Bet.cs
@@ -0,0 +1,100 @@
+﻿namespace RouletteGame.Legacy
+{
+    public abstract class Bet
+    {
+        protected Bet(string name, uint amount)
+        {
+            PlayerName = name;
+            Amount = amount;
+        }
+
+
+        public string PlayerName { get; }
+        public uint Amount { get; }
+
+        public virtual uint WonAmount(Field field)
+        {
+            return 0;
+        }
+    }
+
+    public class FieldBet : Bet
+    {
+        private readonly uint _fieldNumber;
+
+        public FieldBet(string name, uint amount, uint fieldNumber) : base(name, amount)
+        {
+            _fieldNumber = fieldNumber;
+        }
+
+        public override uint WonAmount(Field field)
+        {
+            if (field.Number == _fieldNumber) return 36*Amount;
+            return 0;
+        }
+
+        public override string ToString()
+        {
+            return $"{Amount}$ field bet on {_fieldNumber}";
+        }
+    }
+
+    public class ColorBet : Bet
+    {
+        private readonly uint _color;
+
+        public ColorBet(string name, uint amount, uint color) : base(name, amount)
+        {
+            _color = color;
+        }
+
+        public override uint WonAmount(Field field)
+        {
+            if (field.Color == _color) return 2*Amount;
+            return 0;
+        }
+
+        public override string ToString()
+        {
+            string colorString;
+
+            switch (_color)
+            {
+                case Field.Red:
+                    colorString = "red";
+                    break;
+                case Field.Black:
+                    colorString = "black";
+                    break;
+                default:
+                    colorString = "green";
+                    break;
+            }
+
+            return $"{Amount}$ color bet on {colorString}";
+        }
+    }
+
+    public class EvenOddBet : Bet
+    {
+        private readonly bool _even;
+
+        public EvenOddBet(string name, uint amount, bool even) : base(name, amount)
+        {
+            _even = even;
+        }
+
+        public override uint WonAmount(Field field)
+        {
+            if (field.Even == _even) return 2*Amount;
+            return 0;
+        }
+
+        public override string ToString()
+        {
+            var evenOddString = _even ? "even" : "odd";
+
+            return $"{Amount}$ even/odd bet on {evenOddString}";
+        }
+    }
+}
---
+++ RouletteGame.Legacy/RouletteGame.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RouletteGame.Legacy
+{
+    public class RouletteGame
+    {
+        private readonly List<Bet> _bets;
+        private readonly Roulette _roulette;
+        private bool _roundIsOpen;
+
+        public RouletteGame(Roulette roulette)
+        {
+            _bets = new List<Bet>();
+            _roulette = roulette;
+        }
+
+        public void OpenBets()
+        {
+            Console.WriteLine("Round is open for bets");
+            _roundIsOpen = true;
+        }
+
+        public void CloseBets()
+        {
+            Console.WriteLine("Round is closed for bets");
+            _roundIsOpen = false;
+        }
+
+        public void PlaceBet(Bet bet)
+        {
+            if (_roundIsOpen) _bets.Add(bet);
+            else throw new RouletteGameException("Bet placed while round closed");
+        }
+
+        public void SpinRoulette()
+        {
+            Console.Write("Spinning...");
+            _roulette.Spin();
+            Console.WriteLine("Result: {0}", _roulette.GetResult());
+        }
+
+        public void PayUp()
+        {
+            var result = _roulette.GetResult();
+
+            foreach (var bet in _bets)
+            {
+                var won = bet.WonAmount(result);
+                if (won > 0)
+                    Console.WriteLine("{0} just won {1}$ on a {2}", bet.PlayerName, won, bet);
+            }
+        }
+    }
+
+    public class RouletteGameException : Exception
+    {
+        public RouletteGameException(string s) : base(s)
+        {
+        }
+    }
+}
