c73526105ec5bcc9df3be334ca5624b60ab29115
Tue Jensen <the_tue@hotmail.com>
2018-02-22 11:53:42 +0100

    Added test solution

---
+++ RouletteGame/RouletteGame.Test/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+
+namespace RouletteGame.test
+{
+    [TestFixture]
+    public class RouletteGameTest
+    {
+        [Test]
+        public void BetOpenTrue()
+        {
+            var uut = new RouletteGame.RouletteGame.Legacy();
+
+        }
+
+       
+    }
+
+}
