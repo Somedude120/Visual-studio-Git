89973b9e9c357a6a1d42d0615b794a9cef326a45
Pehmil <pehmil@gmail.com>
2018-03-15 11:21:11 +0100

    Added main program

---
+++ MicrowaveOvenSolution/Program/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MicrowaveOvenClasses.Boundary;
+using MicrowaveOvenClasses.Controllers;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Timer timer = new Timer();
+            Output output = new Output();
+            Display display = new Display(output);
+            PowerTube powerTube = new PowerTube(output);
+            Button btnStart = new Button();
+            Button btnTime = new Button();
+            Button btnCancel = new Button();
+            Door door = new Door();
+            Light light = new Light(output);
+
+            CookController CC = new CookController(timer, display, powerTube);
+            UserInterface ui = new UserInterface(btnStart, btnTime, btnCancel, door, display, light, CC);
+            CC.UI = ui;
+
+            btnStart.Press();
+            CC.StartCooking(20, 1);
+        }
+    }
+}
