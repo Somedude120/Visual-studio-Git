4f34bf4156fa97186b690680b9e9c256e6d0c737
Daniel Hansen <dph1108@gmail.com>
2018-03-15 15:34:40 +0100

    Tried to do integrationstest, didn't work

--- MicrowaveOvenSolution/Program/Program.cs
+++ MicrowaveOvenSolution/Program/Program.cs
@@ -22,14 +22,28 @@ namespace Program
             Door door = new Door();
             Light light = new Light(output);
 
+
+            powerTube.TurnOn(10);
+            timer.Start(timer.TimeRemaining);
+            display.ShowTime(10,0);
+            door.Open();
+            door.Close();
+            
+            light.TurnOn();
+
+            
+            
+
             //Creating cookcontroller without the ui 
             CookController CC = new CookController(timer, display, powerTube);
             //Creating userinterface using property injection
             UserInterface ui = new UserInterface(btnStart, btnTime, btnCancel, door, display, light, CC);
             CC.UI = ui;
 
+            btnTime.Press();
             btnStart.Press();
-            CC.StartCooking(20, 1);
+            btnCancel.Press();
+
         }
     }
 }
