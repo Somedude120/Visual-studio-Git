bbcd6212e73a1c92246b6e9322f79e8e07a9a5f6
Daniel Hansen <dph1108@gmail.com>
2018-03-19 15:27:56 +0100

    Added Integrationtest2 no tests yet

---
+++ MicrowaveOvenSolution/Microwave.Test.Unit/IntegrationTests/IT2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MicrowaveOvenClasses.Boundary;
+using MicrowaveOvenClasses.Interfaces;
+using NUnit.Framework;
+using NSubstitute;
+using MicrowaveOvenClasses.Controllers;
+using Assert = NUnit.Framework.Assert;
+
+
+namespace Microwave.Test.Unit.IntegrationTests
+{
+    [TestFixture]
+    public class IT2
+    {
+        private IOutput _outputDisplay;
+        private IOutput _outputPower;
+
+        private IDisplay _display;
+        private ITimer _timer;
+        private IPowerTube _powerTube;
+        private ICookController _cookController;
+    }
+}
