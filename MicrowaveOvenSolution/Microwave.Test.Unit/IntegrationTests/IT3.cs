21df0d41685906bbae2964dc1b1cc3136e706ee4
Pehmil <pehmil@gmail.com>
2018-03-20 09:02:40 +0100

    Added IT3 and IT4

---
+++ MicrowaveOvenSolution/Microwave.Test.Unit/IntegrationTests/IT3.cs
@@ -0,0 +1,152 @@
+﻿using MicrowaveOvenClasses.Boundary;
+using MicrowaveOvenClasses.Controllers;
+using MicrowaveOvenClasses.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Microwave.Test.Intergration
+{
+    [TestFixture]
+    class IT3
+    {
+        // Classes that have an interface which is under test
+        private Door _door;
+        private Button _powerButton;
+        private Button _startCancelButton;
+        private Button _timeButton;
+
+        // Stubs
+        private IOutput _fakeDisplayOutput;
+        private IOutput _fakePowerTubeOutput;
+        private IOutput _fakeLightOutput;
+
+        // Classes that have been tested
+        private Timer _timer;
+        private Display _display;
+        private PowerTube _powerTube;
+        private Light _light;
+        private CookController _cookController;
+        private UserInterface _userInterface;
+
+        [SetUp]
+        public void Setup()
+        {
+            _door = new Door();
+            _powerButton = new Button();
+            _startCancelButton = new Button();
+            _timeButton = new Button();
+
+            _fakeDisplayOutput = Substitute.For<IOutput>();
+            _fakePowerTubeOutput = Substitute.For<IOutput>();
+            _fakeLightOutput = Substitute.For<IOutput>();
+
+            _timer = new Timer();
+            _display = new Display(_fakeDisplayOutput);
+            _powerTube = new PowerTube(_fakePowerTubeOutput);
+            _light = new Light(_fakeLightOutput);
+            _cookController = new CookController(_timer, _display,
+                _powerTube, _userInterface);
+            _userInterface = new UserInterface(_powerButton,
+                _timeButton, _startCancelButton, _door, _display,
+                _light, _cookController);
+        }
+
+        [Test]
+        public void Door_OpenDoor_OutputIsCalled()
+        {
+            // Act
+            _door.Open();
+
+            // Assert
+            _fakeLightOutput.Received().OutputLine(Arg.Any<string>());
+        }
+
+        [TestCase(1)]
+        [TestCase(5)]
+        public void PowerButton_PressButton_OutputIsCalled(int timesToPress)
+        {
+            // Arrange
+            _door.Open();
+            _door.Close();
+
+            // Act
+            for (int i = 0; i < timesToPress; i++)
+            {
+                _powerButton.Press();
+            }
+
+            // Assert
+            _fakeDisplayOutput.Received(timesToPress).OutputLine(Arg.Any<string>());
+        }
+
+        [TestCase(1)]
+        [TestCase(5)]
+        public void TimerButton_PressButton_OutputIsCalled(int timesToPress)
+        {
+            // Arrange
+            _door.Open();
+            _door.Close();
+            _powerButton.Press();
+
+            // Act
+            for (int i = 0; i < timesToPress; i++)
+            {
+                _timeButton.Press();
+            }
+
+            // Assert
+            // timesToPress + 1 -> as Power-button will generate one call
+            _fakeDisplayOutput.Received(timesToPress + 1).OutputLine(Arg.Any<string>());
+        }
+
+        [Test]
+        public void StartCancelButton_PressButon_OutputIsCalled()
+        {
+            // Arrange
+            _door.Open();
+            _door.Close();
+            _powerButton.Press();
+            _timeButton.Press();
+
+            // Act
+            _startCancelButton.Press();
+
+            // Assert
+            _fakePowerTubeOutput.Received().OutputLine(Arg.Any<string>());
+        }
+
+        [Test]
+        public void Door_OpenDoorWhenRunning_OutputIsCalled()
+        {
+            // Arrange
+            _door.Open();
+            _door.Close();
+            _powerButton.Press();
+            _timeButton.Press();
+            _startCancelButton.Press();
+
+            // Act
+            _door.Open();
+
+            // Assert
+            _fakePowerTubeOutput.Received().OutputLine(Arg.Any<string>());
+        }
+
+        [Test]
+        public void StartCancelButton_PressButtonWhenRunning_OutputIsCalled()
+        {
+            // Arrange
+            _door.Open();
+            _door.Close();
+            _powerButton.Press();
+            _timeButton.Press();
+            _startCancelButton.Press();
+
+            // Act
+            _startCancelButton.Press();
+
+            // Assert
+            _fakePowerTubeOutput.Received().OutputLine(Arg.Any<string>());
+        }
+    }
+}
---
+++ MicrowaveOvenSolution/Microwave.Test.Unit/IntegrationTests/IT4.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MicrowaveOvenClasses.Boundary;
+using MicrowaveOvenClasses.Controllers;
+using MicrowaveOvenClasses.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Microwave.Test.Intergration
+{
+    [TestFixture]
+    class IT4
+    {
+        private IOutput _output;
+
+
+        private IButton _powerButton;
+        private IButton _timerButton;
+        private IButton _startButton;
+
+        private IDoor _door;
+
+        private IUserInterface _userInterface;
+
+        [SetUp]
+        public void Setup()
+        {
+            _powerButton = new Button();
+            _timerButton = new Button();
+            _startButton = new Button();
+
+            _output = new Output();
+
+            IDisplay display = new Display(_output);
+            _userInterface = new UserInterface(_powerButton, _timerButton, _startButton, _door = new Door(), display, new Light(_output), new CookController(new Timer(), display, new PowerTube(_output)));
+        }
+
+        [Test]
+        public void output_openDoor_OutputCorrect()
+        {
+            //Arrange
+            var consoleOutput = new ConsoleOutput();
+
+            //Act
+            _door.Open();
+
+            //Assert
+            Assert.IsTrue(consoleOutput.GetOuput().Contains("Light") && consoleOutput.GetOuput().Contains("turned on"));
+        }
+        [Test]
+        public void output_closeDoor_OutputCorrect()
+        {
+            //Arrange
+            var consoleOutput = new ConsoleOutput();
+
+            //Act
+            _door.Close();
+
+            //Assert
+            Assert.IsTrue(consoleOutput.GetOuput().Contains("Light") && consoleOutput.GetOuput().Contains("turned off"));
+        }
+
+
+
+        [TestCase(1)]
+        [TestCase(5)]
+        public void output_PowerbuttonPress_OutputCorrect(int timesToPress)
+        {
+            //Arrange
+            var consoleOutput = new ConsoleOutput();
+
+            //Act
+            for (int i = 0; i < timesToPress; i++)
+            {
+                _powerButton.Press();
+            }
+
+            //Assert
+            Assert.IsTrue(consoleOutput.GetOuput().Contains("Display") && consoleOutput.GetOuput().Contains($"{timesToPress * 50} W"));
+        }
+
+        [TestCase(1)]
+        [TestCase(5)]
+        public void output_timebuttonPress_OutputCorrect(int timesToPress) //virker ikke
+        {
+            //Arrange
+            var consoleOutput = new ConsoleOutput();
+            //Act
+            for (int i = 0; i < timesToPress; i++)
+            {
+                _timerButton.Press();
+            }
+            //Assert
+            Assert.IsTrue(consoleOutput.GetOuput().Contains("Display") && consoleOutput.GetOuput().Contains($"{timesToPress:D2}:00"));
+        }
+
+
+        public class ConsoleOutput : IDisposable
+        {
+            //From https://stackoverflow.com/questions/2139274/grabbing-the-output-sent-to-console-out-from-within-a-unit-test
+            private StringWriter stringWriter;
+            private TextWriter originalOutput;
+
+            public ConsoleOutput()
+            {
+                stringWriter = new StringWriter();
+                originalOutput = Console.Out;
+                Console.SetOut(stringWriter);
+            }
+
+            public string GetOuput()
+            {
+                return stringWriter.ToString();
+            }
+
+            public void Dispose()
+            {
+                Console.SetOut(originalOutput);
+                stringWriter.Dispose();
+            }
+        }
+    }
+}
