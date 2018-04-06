3f06e2f774f3ae7b70060594231be41bac3c29ae
Daniel Hansen <dph1108@gmail.com>
2018-04-06 13:29:20 +0200

    This is wrong, will be removed

--- Handin3/AirTrafficLibrary/AirTrafficLibrary.Test.Unit/DateTest.cs
+++ Handin3/AirTrafficLibrary/AirTrafficLibrary.Test.Unit/DateTest.cs
@@ -1,8 +1,27 @@
-﻿using System;
+﻿
+using System.Collections.Generic;
+using NUnit.Framework;
 
 namespace AirTrafficLibrary.Test.Unit
 {
-    public class DateTest
+    [TestFixture]
+    class DateTest
     {
+
+        int a;
+        int b;
+
+        [SetUp]
+        public void Setup()
+        {
+            a = 2;
+            b = 2;
+        }
+        [Test]
+        public void test()
+        {
+            Assert.That(a+b,Is.EqualTo(4));
+        }
     }
+
 }
