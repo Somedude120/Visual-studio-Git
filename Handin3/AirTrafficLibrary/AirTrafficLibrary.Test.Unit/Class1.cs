f7dc9f29b78c0dca44d7c5b16918cebf715c9bb7
Daniel Hansen <dph1108@gmail.com>
2018-04-06 13:36:50 +0200

    Test of test

--- Handin3/AirTrafficLibrary/AirTrafficLibrary.Test.Unit/Class1.cs
+++ Handin3/AirTrafficLibrary/AirTrafficLibrary.Test.Unit/Class1.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace AirTrafficLibrary.Test.Unit
 {
-    public class Class1
+    [TestFixture]
+    public class AirTrafficLibraryTestUnit
     {
+        [Test]
+        public void test()
+        {
+            Assert.That(2+2,Is.EqualTo(4));
+        }
     }
 }
