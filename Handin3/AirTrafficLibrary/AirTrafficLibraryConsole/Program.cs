1aa1130161625e98197431e8dd74c6806ee32536
Daniel Hansen <dph1108@gmail.com>
2018-04-08 20:18:38 +0200

    Opgave 5 done

--- Handin3/AirTrafficLibrary/AirTrafficLibraryConsole/Program.cs
+++ Handin3/AirTrafficLibrary/AirTrafficLibraryConsole/Program.cs
@@ -51,14 +51,20 @@ namespace AirTrafficLibrary
 
             string format = "yyyyMMddHHmmssfff";
             string tag = splitInfo[0];
-            string xCoord = splitInfo[1];
-            string yCoord = splitInfo[2];
-            string altitude = splitInfo[3];
+            long xCoord = Convert.ToInt64(splitInfo[1]);
+            long yCoord = Convert.ToInt64(splitInfo[2]);
+            long altitude = Convert.ToInt64(splitInfo[3]);
             string timestamp = splitInfo[4];
             DateTime date = DateTime.ParseExact(timestamp, format, CultureInfo.CreateSpecificCulture("en-US"));
             string dateformat = String.Format(new CultureInfo("en-US"),"{0:MMMM dd'th', yyyy, 'at' HH:mm:ss 'and' fff 'miliseconds'}", date);
 
             string text = $"\nTag: {tag}\nXCoord: {xCoord} Meters\nYCoord: {yCoord} Meters\nAltitude: {altitude} Meters\nTimestamp: {dateformat}";
+            //Rendering af at den ikke vil tage noget med hvis fly har noget specifikke coords
+            //Opgave 5
+            if (xCoord < 10000 && yCoord < 10000 || xCoord > 90000 && yCoord > 90000 || xCoord > 10000 || yCoord > 90000 || altitude < 500 || altitude > 20000)
+            {
+                return "";
+            }
             return text;
 
         }
