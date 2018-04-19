c1b0278bfac66655d89622f46b11924dd2bec5a7
Pehmil <pehmil@gmail.com>
2018-04-19 11:59:44 +0200

    Started work on course heading

--- Handin3/AirTrafficLibrary/AirTrafficLibraryConsole/Program.cs
+++ Handin3/AirTrafficLibrary/AirTrafficLibraryConsole/Program.cs
@@ -13,30 +13,61 @@ namespace AirTrafficLibrary
 {
     class Program
     {
-        
         static void Main()
         {
-            
-            EventHandling Events = new EventHandling();
-            Events.TransponderEventHandler();
+            //EventHandling Events = new EventHandling();
+            //Events.TransponderEventHandler();
 
-            while (true)
-            {
-                
-            }
-            
+            //while (true)
+            //{
+
+            //}
+
+            double x1 = 10, x2 = 90, y1 = 45, y2 = 45;
+            Console.WriteLine(CalculateCourse(x1, x2, y1, y2));
         }
 
-        static public long Velocity(int hour, int minutes, int seconds, int miliseconds)
+        public static long Velocity(int hour, int minutes, int seconds, int miliseconds)
         {
             int oldhour = hour - hour;
             int oldminute = minutes - minutes;
             int oldseconds = seconds - seconds;
             long result = 0;
 
-
             return result;
         }
 
+        public static double CalculateCourse(double x1, double x2, double y1, double y2)
+        {
+            double Rad2Deg = 180.0 / Math.PI;
+            double dx = x2 - x1;
+            double dy = y2 - y1;
+            double angle = Math.Atan2(dy, dx) * Rad2Deg;
+
+            if (angle <= 0)
+            {
+                angle = angle + 360;
+            }
+
+            return angle;
+
+            //var dif = newY - oldY;
+            //Console.WriteLine(dif);
+            //var x = Math.Cos(newX) * Math.Sin(dif);
+            //Console.WriteLine(x);
+
+            ////oldX = 39 -- newX = 38 -- oldY = -94.5 -- newY = -90.200
+
+            ////Y = cos(39.099912) * sin(38.627089) – sin(39.099912) * cos(38.627089) * cos(4.38101)
+
+            //var y = Math.Cos(oldX) * Math.Sin(newX) - Math.Sin(oldX) * Math.Cos(newX) * Math.Cos(dif);
+            //Console.WriteLine(y);
+
+            //var degrees = Math.Atan2(x, y);
+            //degrees = degrees * 180 / Math.PI;
+
+            //return degrees;
+        }
     }
 }
+
