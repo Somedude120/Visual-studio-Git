a8fa030c09bd20444e1d0ec3cd476365428680dc
Daniel Hansen <dph1108@gmail.com>
2018-04-19 12:01:07 +0200

    Started on class for velocity and heading calculations

---
+++ Handin3/AirTrafficLibrary/AirTrafficLibraryConsole/Classes/TrackInfoPosition.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AirTrafficLibraryConsole.Classes
+{
+    public class TrackInfoPosition
+    {
+        private List<TrackObject> _trackList;
+        private List<string> _compareList;
+
+        public void CompareTracks(TrackObject tag)
+        {
+
+            if (tag._tag == _compareList.First())
+            {
+                _compareList.Add(tag._xCoord);
+                _compareList.Add(tag._yCoord);
+            }
+            else
+            {
+                
+            }
+        }
+    }
+}
