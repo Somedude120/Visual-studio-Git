4ffcf3a82479a55bbfe6b040a22023378e776339
Tue Jensen <the_tue@hotmail.com>
2018-04-24 08:36:31 +0200

    Added CalculateVelocity.

---
+++ Handin3/AirTrafficLibrary/AirTrafficLibraryConsole/Classes/Calculatevelocity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirTrafficLibraryConsole.Classes
+{
+    class CalculateVelocity
+    {
+        public double CalcVelocity(double x1, double x2, double y1, double y2)
+        {
+            double x3 = 0;
+            double y3 = 0;
+            double length = 0;
+
+            x3 = x2 - x1;
+
+            y3 = y2 - y1;
+
+            // a^2 + b^2 = c^2 
+            length = Math.Pow(x3, 2) + Math.Pow(y3, 2);
+
+            length = Math.Sqrt(length);
+            
+
+            // mangler at dividere med tiden.
+            return length;
+        }
+    }
+}
