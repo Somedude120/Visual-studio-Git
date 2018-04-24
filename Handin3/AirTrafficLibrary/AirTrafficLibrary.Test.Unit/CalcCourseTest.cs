26c713ef10e1b3610d4adf8004231875d61f81d7
Tue Jensen <the_tue@hotmail.com>
2018-04-24 11:11:58 +0200

    Added ICoords

---
+++ Handin3/AirTrafficLibrary/AirTrafficLibrary.Test.Unit/CalcCourseTest.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using AirTrafficLibrary.Interface;
+using AirTrafficLibraryConsole.Classes;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace AirTrafficLibrary.Test.Unit
+{
+    [TestFixture]
+    public class CalcCourseTest
+    {
+        [Test]
+        public void CalcCource_North()
+        {
+            var uut = new CalculateCource();
+
+        }
+    }
+}
---
+++ Handin3/AirTrafficLibrary/AirTrafficLibraryConsole/Interface/ICoords.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirTrafficLibraryConsole.Interface
+{
+    public interface ICoords
+    {
+        int y { get; set; }
+        int x { get; set; }
+
+    }
+}
