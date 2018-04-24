804c574b5ca3f520adb6e60e8bddecd5f4c65bab
DPH <DPH1108@gmail.com>
2018-04-24 17:04:28 +0200

    Ordnet lidt i print og trackinfo

--- Handin3/AirTrafficLibrary/AirTrafficLibraryConsole/Classes/Print.cs
+++ Handin3/AirTrafficLibrary/AirTrafficLibraryConsole/Classes/Print.cs
@@ -11,8 +11,8 @@ namespace AirTrafficLibraryConsole.Classes
             Console.WriteLine("Y coordinate:\t" + track._yCoord + " meters");
             Console.WriteLine("Altitude:\t" + track._alt + " meters");
             Console.WriteLine("Timestamp:\t" + track._formattedTime);
-            Console.WriteLine("Direction:\t" + track._direction);
-            Console.WriteLine("Velocity:\t" + track._hVelocity);
+            Console.WriteLine("Direction:\t" + track._direction + " degrees");
+            Console.WriteLine("Velocity:\t" + track._hVelocity + "m/s");
             Console.WriteLine();
         }
     }
