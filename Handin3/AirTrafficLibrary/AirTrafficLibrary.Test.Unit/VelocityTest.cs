76c695fa709a63bb634f4c2a14420d619f9f3061
DPH <DPH1108@gmail.com>
2018-04-24 20:42:14 +0200

    Omdøbte CalcCourseTest til VelocityTest, tilsat en unit test for at undersøge om det er korrekt

---
+++ Handin3/AirTrafficLibrary/AirTrafficLibrary.Test.Unit/VelocityTest.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using AirTrafficLibrary.Interface;
+using AirTrafficLibraryConsole.Classes;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace AirTrafficLibrary.Test.Unit
+{
+    [TestFixture]
+    public class VelocityTest
+    {
+        private CalculateVelocity uut;
+        private double _x1, _y1, _x2, _y2, _time;
+
+        [SetUp]
+        public void Setup()
+        {
+            _x1 = 1000;
+            _x2 = 9000;
+            _y1 = 3000;
+            _y2 = 9000;
+            _time = 40;
+            
+            uut = new CalculateVelocity();
+
+        }
+
+        [Test]
+        public void RightSpeedCalculation()
+        {
+            double output = uut.CalcVelocity(_x1, _x2, _y1, _y2, _time);
+
+            Assert.AreEqual(output, 250);
+
+        }
+    }
+}
