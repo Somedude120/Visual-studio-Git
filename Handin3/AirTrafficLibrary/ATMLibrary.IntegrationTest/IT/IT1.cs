066f0ee15ae130415a778864c429b06e21d66fcc
Daniel Hansen <dph1108@gmail.com>
2018-04-25 07:30:48 +0200

    Kunne ikke nå mere. Godnat

--- Handin3/AirTrafficLibrary/ATMLibrary.IntegrationTest/IT/IT1.cs
+++ Handin3/AirTrafficLibrary/ATMLibrary.IntegrationTest/IT/IT1.cs
@@ -1,4 +1,6 @@
-﻿using AirTrafficLibraryConsole.Classes;
+﻿using System;
+using System.Collections.Generic;
+using AirTrafficLibraryConsole.Classes;
 using AirTrafficLibraryConsole.Interface;
 using NSubstitute;
 using NUnit.Framework;
@@ -9,26 +11,40 @@ namespace ATMLibrary.IntegrationTest.IT
     [TestFixture]
     public class IT1
     {
-        private ITransponderReceiver _receiver;
-        private ITrack track;
-        private TrackInfoPosition tracker;
 
-        private FlightMonitor flightmonitor;
-        private DataHandler datahandle;
-        private SeperationEvent separation;
-        private Logger log;
 
+        
+        private EventHandling handler;
+        private ITransponderReceiver receiver;
+        private List<string> _args;
+        private List<string> _splitlist;
+        private ITrack _trackobject;
+        private DateTime date;
+        private Parse parse;
+        private Print _output;
+        private RawTransponderDataEventArgs args;
         [SetUp]
         public void Setup()
         {
-            _receiver = Substitute.For<ITransponderReceiver>();
+            handler = new EventHandling();
+            receiver = Substitute.For<ITransponderReceiver>();
+            _args = new List<string> { "TES123;39045;12932;18000;20151006213456789" };
+            _splitlist = new List<string> { "hans", "15000", "20000", "10000", "10042018" };
+            parse = Substitute.For<Parse>();
+            _trackobject = Substitute.For<ITrack>();
+
         }
 
-        [Test]
-        public void StartupTest()
-        {
-            Assert.AreEqual(2+2,4);
-        }
+        //[Test]
+        //public void HandlingStuff()
+        //{
+        //    List<string> parsedlist;
+        //    parse.ParseFlightInfo("ATR423;39045;12932;14000;20151006213456789").Returns(_splitlist);
+        //    _trackobject = new TrackObject(_splitlist);
+        //    receiver.TransponderDataReady += Raise.EventWith(args);
+            
+        //    Assert.AreEqual(39045, _trackobject._xCoord);
+        //}
 
     }
 }
--- Handin3/AirTrafficLibrary/ATMLibrary.IntegrationTest/IT/IT2.cs
+++ Handin3/AirTrafficLibrary/ATMLibrary.IntegrationTest/IT/IT2.cs
@@ -1,10 +1,30 @@
-﻿using NUnit.Framework;
+﻿using System.Collections.Generic;
+using AirTrafficLibraryConsole.Classes;
+using AirTrafficLibraryConsole.Interface;
+using NUnit.Framework;
 
 namespace ATMLibrary.IntegrationTest.IT
 {
     [TestFixture]
     public class IT2
     {
-        
+        private ITrack track;
+        private TrackInfoPosition tracker;
+        private Parse parse;
+        private Print print;
+        private FlightMonitor flightmonitor;
+        private DataHandler datahandle;
+        private SeperationEvent separation;
+        private List<string> tracklist;
+
+        [SetUp]
+        public void Setup()
+        {
+            tracklist = new List<string>{"First","Second","Third"};
+            track = new TrackObject(tracklist);
+            parse = new Parse();
+            print = new Print(track);
+        }
+
     }
 }
